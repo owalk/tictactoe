cb569ebc56d06c5b4a967643cf9b9ea60eb348c1
Oliver Walker <oliverw56@gmail.com>
2016-11-07 16:58:10 -0500

    Add files via upload

---
+++ board.cpp
@@ -0,0 +1,17 @@
+#include "board.hpp"
+
+board::board(){
+  //set size of board (vector of vectors)
+  int width = 3, int height =3;
+  Board.resize(height);
+  for (int i =0; i < height; i++){
+    Board.[i].resize(width)
+  }
+  return;
+}
+
+board::board(int x, int y){
+
+  return;
+}
+
---
+++ board.hpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using std::vector;
+#ifndef BOARD_H
+#define BOARD_H
+class board {
+public:
+  board();
+  board(int x, int y);
+private:
+  vector<vector<String>> Board;
+};
+#endif
